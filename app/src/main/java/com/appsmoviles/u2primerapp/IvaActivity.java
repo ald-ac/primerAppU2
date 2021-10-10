b2c8f6fd1f4189ba7395f55f3b42a749af268a4a
ald-ac <muriasaldair@hotmail.com>
2021-10-10 01:24:18 -0500

    Apartado IVA Funcional

--- app/src/main/java/com/appsmoviles/u2primerapp/IvaActivity.java
+++ app/src/main/java/com/appsmoviles/u2primerapp/IvaActivity.java
@@ -20,17 +20,34 @@ public class IvaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_iva);
 
         //Instanciar
-        etCosto = findViewById(R.id.et_radio);
+        etCosto = findViewById(R.id.et_costo);
         btnCalcular = findViewById(R.id.btn_calcular);
 
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Validar contenido et
-                if(etCosto.getText().toString().trim() == "") {
-                    Toast.makeText(getApplication(), "Costo invalido", Toast.LENGTH_SHORT).show();
+                if(etCosto.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplication(), "Precio obligatorio", Toast.LENGTH_SHORT).show();
                 } else {
+                    try {
+                        Double costo = Double.parseDouble(etCosto.getText().toString());
+                        Double iva = (costo*0.16);
+                        Double costoOriginal = costo - iva;
 
+                        //Guardar valores para envio
+                        Bundle data = new Bundle();
+                        data.putDouble("costo", costo);
+                        data.putDouble("iva", iva);
+                        data.putDouble("costoOriginal", costoOriginal);
+
+                        //Ir a actividad
+                        Intent intent = new Intent(getApplicationContext(), ResultadoIvaActivity.class);
+                        intent.putExtra("data", data);
+                        startActivity(intent);
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(getApplication(), "Precio invalido", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
--- app/src/main/java/com/appsmoviles/u2primerapp/ResultadoIvaActivity.java
+++ app/src/main/java/com/appsmoviles/u2primerapp/ResultadoIvaActivity.java
@@ -2,13 +2,31 @@ package com.appsmoviles.u2primerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultadoIvaActivity extends AppCompatActivity {
+
+    TextView tvCosto, tvIva, tvTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado_iva);
+
+        Intent intent = getIntent();
+        Bundle data = intent.getBundleExtra("data");
+        Double costoOriginal = data.getDouble("costoOriginal");
+        Double iva = data.getDouble("iva");
+        Double costo = data.getDouble("costo");
+
+        tvCosto = findViewById(R.id.tv_costo);
+        tvIva = findViewById(R.id.tv_iva);
+        tvTotal = findViewById(R.id.tv_total);
+
+        tvCosto.setText(String.format("$%.2f", costoOriginal));
+        tvIva.setText(String.format("$%.2f", iva));
+        tvTotal.setText(String.format("$%.2f", costo));
     }
 }
